f549d5edbfb518b03431572b6b30e76161ada578
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-08-21 20:03:31 +0530

    saving the details and checking fields

--- media/views.py
+++ media/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -6,13 +8,28 @@ def index(request):
 
 
 def signup(request):
-    
+    # created POST method 
     if request.method == "POST":
         username  = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
-
+        
+        #checking fields are correct or not and redirecting again
+        if password == password2:
+            if User.objects.filter(email=email).exits():
+                messages.info(request,'Email already exists')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username already exists')
+                return redirect('signup')
+            else:
+                user=User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                
+        else:
+            messages.info(request,'Password does not match')
+            return redirect('signup')
     else:
         return render(request,'signup.html')
 
