4a7e7aa584b87ded346be375adbf00d4952e9995
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-08-21 21:33:03 +0530

    changed exits spelling to exists

--- media/views.py
+++ media/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render , redirect
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Profile
 
@@ -18,7 +18,7 @@ def signup(request):
         
         #checking fields are correct or not and redirecting again
         if password == password2:
-            if User.objects.filter(email=email).exits():
+            if User.objects.filter(email=email).exists():
                 messages.info(request,'Email already exists')
                 return redirect('signup')
             elif User.objects.filter(username=username).exists():
