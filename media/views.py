bfa28c588b98e7af300934895837a97d5b3aa016
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-08-22 01:03:41 +0530

    login required for authenticated user

--- media/views.py
+++ media/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render , redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
     return render(request,'index.html')
 
@@ -44,7 +46,7 @@ def signup(request):
     else:
         return render(request,'signup.html')
     
-    
+  
 def signin(request):
     
     if request.method == 'POST':
