811106157555a6e99c7e9ddc0b40c96ccfa11347
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-08-21 13:31:38 +0530

    bio field maxlength changed to 150

--- media/models.py
+++ media/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
-    bio = models.TextField(max_length=100,blank=True)
+    bio = models.TextField(max_length=150,blank=True)
     profileimg = models.ImageField(upload_to='profile_images',default='blank-profile-picture.png')
     location = models.CharField(max_length=50,blank=True)
     
