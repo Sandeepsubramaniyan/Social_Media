2fa873e3b3d73c94f953461e09ed94fe6365e2c7
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-08-23 05:22:26 +0530

    moved settings path  to line 9

--- media/urls.py
+++ media/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('signup/',views.signup,name='signup'),
     path('signin/',views.signin,name='signin'),
+    path('settings/',views.settings,name='settings'),
     path('upload/',views.upload,name='upload'),
     path('logout/',views.logout,name='logout'),
-    path('settings',views.settings,name='settings'),
+    
     
 ]
