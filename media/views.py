46e0f291b02949223db5d3f706d6f29761eb0854
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-08-22 20:09:59 +0530

    creating view to upload picture to home

--- media/views.py
+++ media/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -11,7 +11,19 @@ def index(request):
 
 @login_required(login_url='signin')
 def upload(request):
-    pass
+    
+    if request.method == "POST":
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+        
+        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+        
+        return redirect('/')
+    else:
+        return redirect('/')
+    
 
 @login_required(login_url='signin')
 def settings(request):
