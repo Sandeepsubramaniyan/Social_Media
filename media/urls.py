7cc769262367b8c016d1cc38be37e9e86ecd135f
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-08-22 19:32:13 +0530

    creating url path for upload view

--- media/urls.py
+++ media/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('signup/',views.signup,name='signup'),
     path('signin/',views.signin,name='signin'),
+    path('upload/',views.upload,name='upload'),
     path('logout/',views.logout,name='logout'),
-    path('settings',views.settings,name='settings')
+    path('settings',views.settings,name='settings'),
+    
 ]
