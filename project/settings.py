bb8ebf5da5fe0a1b8a84017b02b1ead3c9ff3a30
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-08-22 18:52:24 +0530

    changed URL and ROOT and slash in data

--- project/settings.py
+++ project/settings.py
@@ -128,5 +128,5 @@ STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DATA_URL = '/data'
-DATA_ROOT = os.path.join(BASE_DIR,'data')
+MEDIA_URL = '/data/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'data')
