02cd3c0fe49e3044b0c28e9fc0925cd42c253b86
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-08-23 06:29:39 +0530

    removed slash from upload path

--- media/urls.py
+++ media/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('signup/',views.signup,name='signup'),
     path('signin/',views.signin,name='signin'),
     path('settings/',views.settings,name='settings'),
-    path('upload/',views.upload,name='upload'),
+    path('upload',views.upload,name='upload'),
     path('logout/',views.logout,name='logout'),
     
     
