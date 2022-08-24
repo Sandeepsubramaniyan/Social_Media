38da56cd4bd4c9ed65bd5e544e0193e5ba707bec
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-08-24 16:50:34 +0530

    registering the FollowerCount model in adminpanel

--- media/admin.py
+++ media/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import  Post, Profile, LikePost
+from .models import  FollowerCount, Post, Profile, LikePost
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(LikePost)
+admin.site.register(FollowerCount)
+
 
